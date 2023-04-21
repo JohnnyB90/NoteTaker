c1ed5fd93f13ca322de5acd8881da684409f9ca3
Johnny Bonee <johnny.bonee@ultimaterestoration.com>
2023-04-21 15:33:22 -0400

    Corrected notetakes not adding notes by adding fs functionality to read/write/delete notes. Tied this in with the index.js to make the backend meet the front end. Installed the UUID package and added ID's to each note added

--- Develop/routes/apiroutes.js
+++ Develop/routes/apiroutes.js
@@ -1,7 +1,52 @@
 const apiRouter = require('express').Router();
-// Get route to display notes and open note taking page
-apiRouter.get('/notes', (req, res) => {
-    res.send('Hello')
-  });
+const fs = require('fs');
+const path = require('path');
+const { v4: uuidv4 } = require('uuid'); // For generating unique IDs, install with `npm install uuid`
 
-  module.exports = apiRouter;
+// Function to read and write notes to the db.json file
+const notesFilePath = path.join(__dirname, '../db/db.json');
+
+function readNotes() {
+  const notesData = fs.readFileSync(notesFilePath, 'utf-8');
+  return JSON.parse(notesData);
+}
+
+function writeNotes(notes) {
+  fs.writeFileSync(notesFilePath, JSON.stringify(notes, null, 2));
+}
+
+// GET route for fetching the list of notes
+apiRouter.get('/notes', (req, res) => {
+  const notes = readNotes();
+  res.json(notes);
+});
+
+// DELETE route for deleting notes
+apiRouter.delete('/notes/:id', (req, res) => {
+  const noteId = req.params.id;
+  let notes = readNotes();
+  notes = notes.filter(note => note.id !== noteId);
+  writeNotes(notes);
+  res.status(200).json({ message: 'Note deleted successfully.' });
+});
+
+
+// POST route for adding notes
+apiRouter.post('/notes', (req, res) => {
+  const newNote = {
+    id: uuidv4(),
+    title: req.body.title,
+    text: req.body.text,
+  };
+
+  // Push notes into newNote and write it.
+  const notes = readNotes();
+  notes.push(newNote);
+  writeNotes(notes);
+
+  
+
+  res.status(201).json(newNote);
+});
+
+module.exports = apiRouter;
--- Develop/routes/index.js
+++ Develop/routes/index.js
@@ -1,12 +1,12 @@
 const express = require('express');
 
-//Import our modular routers for /apiroutes and /htmlroutes
+// Import our modular routers for /apiroutes and /htmlroutes
 const apiRouter = require('./apiroutes');
 const htmlRouter = require('./notesroutes');
 
-//import diagnositcs route
 const app = express();
 
-app.use('/notes', htmlRouter);
+app.use('/api', apiRouter); // Use the apiRouter for /api routes
+app.use('/', htmlRouter); // Use the htmlRouter for /notes route
 
 module.exports = app;

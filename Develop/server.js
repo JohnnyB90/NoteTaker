e538a0ffe569698d94b0790e8a37f802924e1a9e
Johnny <117334322+JohnnyB90@users.noreply.github.com>
2023-04-22 01:13:30 -0400

    Fixed the readme file to represent this application, also got rid of the extra assets folder containing demo photos

--- Develop/server.js
+++ Develop/server.js
@@ -6,11 +6,12 @@ const apiRoutes = require('./routes/apiroutes');
 // Create a new instance of an Express application
 const app = express();
 
-// Set the application to listen on either the specified port or port 3001
-const PORT = process.env.PORT || 3001;
-app.listen(PORT, () => {
-  console.log(`Server is listening on port ${PORT}`);
-});
+// Enables parsing of bodies coming through or when adding new notes in this case
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Allows express to access and use the files in the public folder such as index.js, index.html, css files etc.
+app.use(express.static('public'));
 
 // Use the htmlRoutes router for requests to the root route
 app.use('/', htmlRoutes);
@@ -18,19 +19,8 @@ app.use('/', htmlRoutes);
 // Use the apiRoutes router for requests to the /api route
 app.use('/api', apiRoutes);
 
-
-
-// Enables parsing of bodies coming through or when adding new notes in this case
-app.use(express.json());
-//
-app.use(express.urlencoded({ extended: true }));
-// Allows express to access and use the files in the public folder such as index.js, index.html, css files etc.
-app.use(express.static('public'));
-// Gives access to the api routes using the path /api
-app.use('/api', apiRoutes);
-// Gives access to the homepage using htmlRoutes
-app.use('/', htmlRoutes);
-// When successfully listening, console log the message below
+// Set the application to listen on either the specified port or port 3001
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
+  console.log(`Server is listening on port ${PORT}`);
 });
